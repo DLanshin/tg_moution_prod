f7c91624e39cdc328b547a31ebef0e55ada235b7
DaniilLanshinSPB <d.lanshin@finlead.team>
2022-07-25 11:12:36 +0300

    re-relese

---
+++ temp/mocksData.js
@@ -0,0 +1,246 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+},
+
+'head': {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    }
+}
+,
+
+'header': {
+    main: {
+        description: {
+            title: "Home & Kitchen Remodeling <br />in Sacramento",
+            text: "We are trusted by thousands of homeowners <br />Sacramento's #1 one-stop shop Design & Build Firm <br />Design and development of residential homes since 1997"
+        },
+        picture: {
+            src: 'static/img.lazy/content/hero-img.lazy',
+        },
+        ctaForm: {
+            title: "Receive $500 OFF",
+            subtitle: "When You Request Online",
+            buttonText: "Request An Estimate",
+            smallText: "claim offer by June 30, <br />2021"
+        }
+    }
+}
+,
+
+'nav': {
+    primaryListRu: [
+        {
+            title: 'Цена',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "pricing"
+        },
+        {
+            title: 'Преимущества',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "benefits"
+        },
+        {
+            title: 'Примеры рассылок',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "examples"
+        },
+        {
+            title: 'Отзывы',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "reviews",
+        },
+        {
+            title: 'Наш канал',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "contact-us"
+        },
+    ],
+    primaryListEn: [
+        {
+            title: 'Price',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "pricing"
+        },
+        {
+            title: 'Advantages',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "benefits"
+        },
+        {
+            title: 'Examples of mailing',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "examples"
+        },
+        {
+            title: 'Reviews',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "reviews",
+        },
+        {
+            title: 'Our channel',
+            href: 'javascript:void(0)',
+            itemClassMod: '',
+            scrollToElem: "contact-us"
+        },
+
+    ]
+
+}
+,
+
+__iconsData: {
+    
+        'arrow-right': {
+            width: '105px',
+            height: '24px'
+        },
+    
+        'audience-path': {
+            width: '337px',
+            height: '601px'
+        },
+    
+        'benefits-path-left': {
+            width: '445px',
+            height: '695px'
+        },
+    
+        'benefits-path-right': {
+            width: '557px',
+            height: '695px'
+        },
+    
+        'close-icon': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'discord-icon': {
+            width: '62px',
+            height: '62px'
+        },
+    
+        'examples-path': {
+            width: '621px',
+            height: '593px'
+        },
+    
+        'hero-path': {
+            width: '784px',
+            height: '674px'
+        },
+    
+        'instagram-icon': {
+            width: '62px',
+            height: '62px'
+        },
+    
+        'lang-en': {
+            width: '21px',
+            height: '21px'
+        },
+    
+        'lang-ru': {
+            width: '21px',
+            height: '21px'
+        },
+    
+        'logo-second': {
+            width: '162px',
+            height: '42px'
+        },
+    
+        'logo': {
+            width: '162px',
+            height: '42px'
+        },
+    
+        'menu-icon': {
+            width: '20px',
+            height: '14px'
+        },
+    
+        'play': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'prices-path': {
+            width: '953px',
+            height: '499px'
+        },
+    
+        'reviews-path': {
+            width: '308px',
+            height: '398px'
+        },
+    
+        'slider-arrow': {
+            width: '64px',
+            height: '65px'
+        },
+    
+        'telegram-icon': {
+            width: '62px',
+            height: '62px'
+        },
+    
+        'video-path-2': {
+            width: '611px',
+            height: '671px'
+        },
+    
+        'video-path': {
+            width: '577px',
+            height: '682px'
+        },
+    
+        'views-icon': {
+            width: '15px',
+            height: '9px'
+        },
+    
+        'vk-icon': {
+            width: '62px',
+            height: '62px'
+        },
+    
+        'write-path': {
+            width: '611px',
+            height: '671px'
+        },
+    
+},
+
+__pages: [{
+                name: 'index-en',
+                href: 'index-en.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             }]
