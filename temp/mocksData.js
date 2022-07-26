bec5fb7a7d15744a7f2d41012784e90023da431e
DaniilLanshinSPB <d.lanshin@finlead.team>
2022-07-26 19:32:09 +0300

    build

--- temp/mocksData.js
+++ temp/mocksData.js
@@ -18,7 +18,27 @@ _template: {
 
 'head': {
     defaults: {
-        title: 'default title',
+        title: 'TG Motion - Сервис продвижения и рассылки рекламы',
+        description: '',
+        image: 'https://tgmotion.top/icon-192x192.png',
+        host: 'https://tgmotion.top/',
+        telegram: 'https://t.me/TGMotion_bot',
+        useSocialMetaTags: true
+    },
+    ru: {
+        title: 'TG Motion - Сервис продвижения и рассылки рекламы',
+        description: 'Профессиональный сервис по раскрутке и продвижению в социальных сетях. Накрутка реальных подписчиков. Быстрая, безопасная накрутка лайков, подписчиков и друзей ВКонтакте, Инстаграм, Тик Ток, Twitter, Facebook, Youtube онлайн без заданий с гарантией .',
+        image: 'https://tgmotion.top/icon-192x192.png',
+        host: 'https://tgmotion.top/',
+        telegram: 'https://t.me/TGMotion_bot',
+        useSocialMetaTags: true
+    },
+    en:{
+        title: 'TG Motion - Advertising promotion and mailing service',
+        description: 'Professional service for promotion and promotion in social networks. Cheat real subscribers. Fast, safe boost of likes, followers and friends on VKontakte, Instagram, Tik Tok, Twitter, Facebook, Youtube online without tasks with a guarantee.',
+        image: 'https://tgmotion.top/icon-192x192.png',
+        host: 'https://tgmotion.top/en',
+        telegram: 'https://t.me/TGMotion_bot',
         useSocialMetaTags: true
     }
 }
